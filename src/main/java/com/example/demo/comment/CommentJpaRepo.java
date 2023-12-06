3a12656d79ffd82c4de63b9d515b718500bfbb03
taeyun1215 <taeyun1215@naver.com>
2023-12-06 23:02:28 +0900

    fix : del comment entity

--- src/main/java/com/example/demo/comment/CommentJpaRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentJpaRepo extends JpaRepository<CommentJpaEntity, Long> {
-
-}
