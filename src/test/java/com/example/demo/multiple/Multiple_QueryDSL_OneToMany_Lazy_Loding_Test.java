3b819611c721e3298c21ae15cecc6bbe7276d5e6
taeyun1215 <taeyun1215@naver.com>
2023-12-13 22:56:17 +0900

    test : add test case 4(Pagination), 5(Streaming)

--- src/test/java/com/example/demo/multiple/Multiple_QueryDSL_OneToMany_Lazy_Loding_Test.java
+++ src/test/java/com/example/demo/multiple/Multiple_QueryDSL_OneToMany_Lazy_Loding_Test.java
@@ -1,9 +1,7 @@
 package com.example.demo.multiple;
 
-import com.example.demo.multiple.category.Category;
 import com.example.demo.multiple.category.CategoryRepo;
 import com.example.demo.multiple.category.QCategory;
-import com.example.demo.multiple.customer.Customer;
 import com.example.demo.multiple.customer.CustomerRepo;
 import com.example.demo.multiple.customer.QCustomer;
 import com.example.demo.multiple.product.*;
@@ -21,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Transactional
 @SpringBootTest
@@ -84,8 +83,8 @@ public class Multiple_QueryDSL_OneToMany_Lazy_Loding_Test {
     }
 
     @Test
-    @DisplayName("QueryDSL과 Fetch Join을 사용하여 N+1 문제 해결")
-    public void testFetchJoinWithQueryDSL1() {
+    @DisplayName("QueryDSL을 사용할 때 발생하는 N+1 문제를 확인합니다.")
+    public void test1() {
         em.flush();
         em.clear();
         System.out.println("------------ 영속성 컨텍스트 비우기 -----------\n");
@@ -108,7 +107,7 @@ public class Multiple_QueryDSL_OneToMany_Lazy_Loding_Test {
 
     @Test
     @DisplayName("QueryDSL과 Fetch Join을 사용하여 N+1 문제 해결")
-    public void testFetchJoinWithQueryDSL2() {
+    public void test2() {
         em.flush();
         em.clear();
         System.out.println("------------ 영속성 컨텍스트 비우기 -----------\n");
@@ -133,7 +132,7 @@ public class Multiple_QueryDSL_OneToMany_Lazy_Loding_Test {
 
     @Test
     @DisplayName("Product 안에 category 데이터 다 가져오기")
-    public void testFetchJoinWithQueryDSL3() {
+    public void test3() {
         em.flush();
         em.clear();
         System.out.println("------------ 영속성 컨텍스트 비우기 -----------\n");
@@ -142,33 +141,97 @@ public class Multiple_QueryDSL_OneToMany_Lazy_Loding_Test {
 
         queryFactory
                 .select(Projections.fields(ProductDetails1.class,
-                        product.id,
-                        product.name,
-                        product.price,
-                        category))
+                        product.id.as("productId"),
+                        product.name.as("productName"),
+                        product.price.as("productPrice"),
+                        product.category.as("category")))
                 .from(product)
+                .where(product.price.gt(0))
+                .orderBy(product.name.asc())
                 .fetch();
     }
 
     @Test
     @DisplayName("Product 안에 category Id 만 가져오기")
-    public void testFetchJoinWithQueryDSL4() {
+    public void test4() {
         em.flush();
         em.clear();
         System.out.println("------------ 영속성 컨텍스트 비우기 -----------\n");
 
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
         QProduct product = QProduct.product;
-        QCategory category = QCategory.category;
 
-        queryFactory
+        List<ProductDetails2> results = queryFactory
                 .select(Projections.fields(ProductDetails2.class,
-                        product.id,
-                        product.name,
-                        product.price,
-                        category.id))
+                        product.id.as("productId"),
+                        product.name.as("productName"),
+                        product.price.as("productPrice"),
+                        product.category.id.as("categoryId")))
                 .from(product)
+                .where(product.price.gt(0))
+                .orderBy(product.name.asc())
+                .limit(1000)
                 .fetch();
 
+        for (ProductDetails2 detail : results) {
+            System.out.println("Product: " + detail.getProductName());
+        }
+    }
+
+    @Test
+    @DisplayName("Product 안에 category Id 만 가져오기 with Pagination")
+    public void test5() {
+        em.flush();
+        em.clear();
+        System.out.println("------------ 영속성 컨텍스트 비우기 -----------\n");
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QProduct product = QProduct.product;
+
+        int pageNumber = 1; // 페이지 번호 (예: 1)
+        int pageSize = 20; // 페이지 당 항목 수 (예: 10)
+        long offset = (pageNumber - 1) * pageSize; // 페이지 시작 위치 계산
+
+        List<ProductDetails2> results = queryFactory
+                .select(Projections.fields(ProductDetails2.class,
+                        product.id.as("productId"),
+                        product.name.as("productName"),
+                        product.price.as("productPrice"),
+                        product.category.id.as("categoryId")))
+                .from(product)
+                .where(product.price.gt(0))
+                .orderBy(product.name.asc())
+                .offset(offset) // 페이지 시작 위치 설정
+                .limit(pageSize) // 페이지 당 항목 수 설정
+                .fetch();
+    }
+
+    @Test
+    @DisplayName("Product 안에 category Id 만 가져오기 with Streaming")
+    public void test6() {
+        em.flush();
+        em.clear();
+        System.out.println("------------ 영속성 컨텍스트 비우기 -----------\n");
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QProduct product = QProduct.product;
+
+
+        try (Stream<ProductDetails2> stream = queryFactory
+                .select(Projections.fields(ProductDetails2.class,
+                        product.id.as("productId"),
+                        product.name.as("productName"),
+                        product.price.as("productPrice"),
+                        product.category.id.as("categoryId")))
+                .from(product)
+                .where(product.price.gt(0))
+                .orderBy(product.name.asc())
+                .stream()) {
+
+            stream.forEach(productDetails2 -> {
+                // 각 Product 객체에 대한 처리
+                System.out.println(productDetails2.getProductName());
+            });
+        }
     }
 }
